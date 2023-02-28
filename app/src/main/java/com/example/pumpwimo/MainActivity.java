97d752fda3a090cf8f1c1bf8da4fa0a87bb32c1b
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-03-01 01:49:11 +0300

    It is PumpWiMo

--- app/src/main/java/com/example/pumpwimo/MainActivity.java
+++ app/src/main/java/com/example/pumpwimo/MainActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
+
 
 import com.example.pumpwimo.databinding.ActivityMainBinding;
 
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.createAccount.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(i);
+        });
+
+        binding.enter.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+        });
     }
 }
