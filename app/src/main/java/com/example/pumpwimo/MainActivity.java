32837e119782ed59c222b583fb3bbc3b6c3c6c14
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-03-02 08:33:02 +0300

    It is PumpWiMo

--- app/src/main/java/com/example/pumpwimo/MainActivity.java
+++ app/src/main/java/com/example/pumpwimo/MainActivity.java
@@ -1,31 +1,29 @@
 package com.example.pumpwimo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 
 import com.example.pumpwimo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
+    private ViewPager mSladeViewPager;
+
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.createAccount.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
-            startActivity(i);
-        });
-
-        binding.enter.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(i);
-        });
+        mSladeViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        linearLayout = (LinearLayout) findViewById(R.id.dotsLayout);
     }
 }
