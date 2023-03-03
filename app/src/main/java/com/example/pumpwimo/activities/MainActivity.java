880d4b913df836ac8d4338d6e4be89729aeae053
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-03-04 02:05:34 +0300

    It is PumpWiMo

--- app/src/main/java/com/example/pumpwimo/activities/MainActivity.java
+++ app/src/main/java/com/example/pumpwimo/activities/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.pumpwimo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.constraintlayout.helper.widget.Carousel;
 
+import android.content.Intent;
+import android.graphics.Movie;
 import android.os.Bundle;
-import android.view.View;
 
+import com.example.pumpwimo.R;
 import com.example.pumpwimo.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    private final CardView[] cardViews = {
-            binding.cardView1,
-            binding.cardView2,
-            binding.cardView3,
-            binding.cardView4,
-            binding.cardView5,
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +21,16 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.carousel.setAdapter(new Carousel.Adapter() {
-            @Override
-            public int count() {
-                return cardViews.length;
-            }
+        binding.button1.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(i);
+            finish();
+        });
 
-            @Override
-            public void populate(View view, int index) {
-
-            }
-
-            @Override
-            public void onNewItem(int index) {
-
-            }
+        binding.button2.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
         });
     }
 }
