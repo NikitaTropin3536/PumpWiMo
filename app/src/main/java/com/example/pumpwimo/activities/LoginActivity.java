d1cbd31e608ad7dc17b6600fd35938e0053703e4
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-03-05 01:59:53 +0300

    It is PumpWiMo

--- app/src/main/java/com/example/pumpwimo/activities/LoginActivity.java
+++ app/src/main/java/com/example/pumpwimo/activities/LoginActivity.java
@@ -2,15 +2,34 @@ package com.example.pumpwimo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.pumpwimo.R;
+import com.example.pumpwimo.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // войти
+        binding.buttonLogin.setOnClickListener(v -> {
+            Intent i = new Intent(LoginActivity.this, BoardActivity.class);
+            startActivity(i);
+            finish();
+        });
+
+        // создать аккаунт
+        binding.buttonCreateAccount.setOnClickListener(v -> {
+            Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+            startActivity(i);
+            finish();
+        });
+
+        // совпадение или нет
     }
 }
